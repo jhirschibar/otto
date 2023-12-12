87c57d8e1f5cdc19410b183782f95525b86ce50c
jhirschibar <jhirschibar@gmail.com>
2023-12-12 08:48:21 -0500

    feat: first http request in rust!

--- otto-listener/src/main.rs
+++ otto-listener/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// Import necessary packages
+#[macro_use] extern crate rocket;
+
+// Define the route handler for `/test`
+#[get("/test")]
+fn test() -> &'static str {
+    "This is a test endpoint"
+}
+
+// Define the main function to start the Rocket server with the `/test` route
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![test])
 }
--- sinch-client/src/main.rs
+++ sinch-client/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::header::HeaderValue;
+use reqwest::Client;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://us.sms.api.sinch.com/xms/v1/{}/batches"; //add project id
+
+    let headers = {
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert(
+            reqwest::header::CONTENT_TYPE,
+            HeaderValue::from_static("application/json"),
+        );
+        headers
+    };
+
+    let client = Client::new();
+
+    let response = client
+        .get(url)
+        .headers(headers)
+        .header("Authorization", "Bearer ") // add bearer token
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("{:#?}", response);
+
+    Ok(())
 }
+
+    
